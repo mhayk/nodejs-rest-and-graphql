97b9c4400386d062b7ac0b986fdd628f48e1bb48
Mhayk Whandson <hi@mhayk.com>
2021-08-17 22:32:06 +0100

    Controller to create a user and get posts have been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/modules/posts/controllers/GetPostsByUserController.ts
@@ -0,0 +1,16 @@
+import { Response } from "express";
+import { container } from "tsyringe";
+import { GetPostByUserService } from "../services/GetPostsByUserService";
+
+class GetPostsByUserController {
+    async handle(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const getPostByUserService = container.resolve(GetPostByUserService)
+        const posts = await getPostByUserService.execute(id);
+
+        return response.json(posts);
+    }
+}
+
+export { GetPostsByUserController };
---
+++ src/modules/users/controllers/CreateUserController.ts
@@ -0,0 +1,15 @@
+import { container } from "tsyringe";
+import { CreateUserService } from "../services/CreateUserService";
+
+class CreateUserController {
+    async handle(request: Request, response: Response) {
+        const { name, username } = request.body;
+
+        const createUserService = container.resolve(CreateUserService)
+        const user = await createUserService.execute({ name, username });
+
+        return response.json(user);
+    }
+}
+
+export { CreateUserController };
